1a3fdcec00b6c7a0b65a1ac051d9aed301f736ec
Divyasri <divyasrisowjanya@gmail.com>
2025-07-04 16:54:28 +0530

    java fun

---
+++ Java_fundamentals/src/java_fundamentals/Question2.java
@@ -0,0 +1,14 @@
+package java_fundamentals;
+import java.util.*;
+public class Question2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a+b;
+		System.out.println("The sum of " +a+ " and "+b+ " is " +c);
+		
+	}
+
+}
