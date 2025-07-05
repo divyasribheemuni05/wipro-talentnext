dfcdbc08accbe71bce43626c915e4513ab5ea369
Divyasri <divyasrisowjanya@gmail.com>
2025-07-05 10:47:59 +0530

    Java_fundamentals

---
+++ Java_fundamentals/src/java_fundamentals/Question10.java
@@ -0,0 +1,19 @@
+package java_fundamentals;
+import java.util.*;
+public class Question10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		int n=s.nextInt();
+		int re=0;
+		while(n!=0) {
+			int d=n%10;
+			re=re*10+d;
+			n=n/10;
+			
+		}
+		System.out.println(re);
+	}
+
+}
